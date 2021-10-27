72c9a1df8f4dbbf42b7b01ae919ab940a9bd1062
sirine27 <mathlouthi.sirine124@gmail.com>
2021-10-27 22:20:39 +0200

    modify

--- frontend/src/Components/Pages/Add.js
+++ frontend/src/Components/Pages/Add.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {Form, Button} from "react-bootstrap";
+import { Button, Form } from 'semantic-ui-react'
 import { editMovie, postMovie } from '../../JS/actions/movie'
 import { Link } from 'react-router-dom';
 
@@ -31,61 +31,61 @@ const handleChange = (e)=>{
     return(
         <div style={{"position":"absolute","top":"100px","right":"300px","width": "500px", "color":"red", "fontSize":"13px", "backgroundColor":"#181818","border":"1px"}}>
      <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-  
-      <Form.Label>Title: </Form.Label>
-          
-          <Form.Control type="text" placeholder='Title' name="title" value={movie.title} onChange={handleChange}  />
-   
-  </Form.Group>
+ 
+  <Form.Field>
+      <label>Title</label>
+      <input placeholder='Title' name="title" value={movie.title} onChange={handleChange}/>
+    </Form.Field>
 
+     
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Date: </Form.Label>
-    <Form.Control type="text" placeholder='Date' name="date" value={movie.date} onChange={handleChange} />
-  </Form.Group>
-
-
+  <Form.Field>
+      <label>Date</label>
+      <input placeholder='Date' name="date" value={movie.date} onChange={handleChange} />
+    </Form.Field>
 
 
       
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Image: </Form.Label>
-    <Form.Control type="text" placeholder='Image' name="image" value={movie.image} onChange={handleChange} />
-  </Form.Group>
-
-
-       
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Discription: </Form.Label>
-    <Form.Control type="text" placeholder='Discription' name="description" value={movie.description} onChange={handleChange} />
-  </Form.Group>
-
-      
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Type: </Form.Label>
-    <Form.Control type="text" placeholder='Type' name="type" value={movie.type} onChange={handleChange} />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Link: </Form.Label>
-    <Form.Control type="text" placeholder='link' name="link" value={movie.link} onChange={handleChange} />
-  </Form.Group>
-
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Rating:</Form.Label>
-    <Form.Control type="text"  name="rating" value={movie.rating} onChange={handleChange} />
-  </Form.Group>
     
 
+  <Form.Field>
+      <label>Image</label>
+      <input placeholder='Image' name="image" value={movie.image} onChange={handleChange} />
+    </Form.Field>
+       
 
-  
+
+
+  <Form.Field>
+      <label>Description</label>
+      <input placeholder='Description' name="description" value={movie.description} onChange={handleChange} />
+    </Form.Field>
+
+     
+
+  <Form.Field>
+      <label>Type</label>
+      <input placeholder='Type' name="type" value={movie.type} onChange={handleChange} />
+    </Form.Field>
+
+
+
+  <Form.Field>
+      <label>Link</label>
+      <input placeholder='Link' name="link" value={movie.link} onChange={handleChange} />
+    </Form.Field>
+
+    
+  <Form.Field>
+      <label>Rating</label>
+      <input placeholder='Rating' name="rating" value={movie.rating} onChange={handleChange} />
+    </Form.Field>
+
+    <Link to="/movieList">
              <Button style={{"position":"fixed","bottom":"40px","right": "0", "color":"red", "fontSize":"13px", "backgroundColor":"#181818", "width": "120px","border":"2px solid red"}} variant="primary" type="submit" onClick={handleMovie}>
              {edit? "Edit" : "Add Movie"}
            </Button>
+           </Link>
          </Form>
 
 <Link to="/dashboard"> <Button style={{"position":"fixed","bottom":"80px","right": "0", "color":"red", "fontSize":"13px", "backgroundColor":"#181818", "width": "120px","border":"2px solid red"}}>Go To Home</Button></Link>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './JS/store/Store';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css'
 
 import App from './App';
 
